578b6d14c1be20999f845d90f89dbe8f84087cc8
“Bunny <BunnyRabbit_aws@gmail.com>
2025-02-19 19:41:39 -0800

    removed DocDB and using Dynamo DB instead

--- source/processInputData.py
+++ source/processInputData.py
@@ -1,9 +1,6 @@
 import boto3
 import json
-from pymongo import MongoClient
-import ssl
 import yaml
-import json
 from decimal import Decimal
 
 def float_to_decimal(obj):
@@ -34,9 +31,12 @@ s3_key = s3_config['key']
 print('initializing S3 client..')
 s3 = boto3.client('s3')
 
+# Extract DDB configuration
+ddb_config = config['dynamodb']
+ddb_table_name = ddb_config['table_name']
+
 # Initialize DDB
 dynamodb = boto3.resource('dynamodb')
-ddb_table_name = "subscription_table"
 ddb_table = dynamodb.Table(ddb_table_name)
 
 # Read JSONL file from S3
@@ -66,70 +66,3 @@ for line in content.splitlines():
 
 print(f"Inserted Objects: {insert_count }")
 print("Data insertion complete.")
-
-
-"""
-# Extract DocumentDB configuration
-db_config = config['documentdb']
-print('DB Config:', db_config)
-docdb_host = db_config['host']
-docdb_port = db_config['port']
-docdb_username = db_config['username']
-docdb_password = db_config['password']
-docdb_database = db_config['database']
-docdb_collection = db_config['collection']
-
-
-
-# Initialize DocumentDB client
-print('initializing DocumentDB client..')
-client = MongoClient(
-    host=docdb_host,
-    port=docdb_port,
-    username=docdb_username,
-    password=docdb_password,
-    tls=True,
-    tlsAllowInvalidCertificates=True
-)
-
-print('Connecting to DB..')
-db = client[docdb_database]
-print('Connecting to Collection..')
-collection = db[docdb_collection]
-
-# Print the number of documents
-document_count = collection.count_documents({})
-print(f"Number of documents in the collection: {document_count}")
-
-# Print the contents of each document
-print("\nDocuments in the collection:")
-for document in collection.find():
-    print(document)
-
-
-# Read JSONL file from S3
-response = s3.get_object(Bucket=s3_bucket, Key=s3_key)
-content = response['Body'].read().decode('utf-8')
-
-# Process each line (JSON object) in the file
-print('Processing JSON data..')
-for line in content.splitlines():
-    try:
-        json_object = json.loads(line)
-        # Insert the JSON object into DocumentDB
-        print(f"Inserting: {json_object}")
-        collection.insert_one(json_object)
-        print(f"Inserted...")
-    except json.JSONDecodeError as e:
-        print(f"Error decoding JSON: {e}")
-    except Exception as e:
-        print(f"Error inserting document: {e}")
-
-print("Data insertion complete.")
-
-
-
-print('Closing connection to DB..')
-client.close()
-
-"""
